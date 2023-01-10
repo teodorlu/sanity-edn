ce5b3ec3f2927ef4a877be406aa085fde03233aa
Teodor Heggelund <git@teod.eu>
2023-01-10 16:56:06 +0100

    Update :parameters->:params in docs

--- example.clj
+++ example.clj
@@ -1,6 +1,8 @@
 (ns yourapp
   (:require [teodorlu.sanity-edn :as sanity]))
 
+(defn lookup-your-sanity-secret [] ,,,)
+
 (def client (sanity/client {:project-id "abcd1234"
                             :api-version "v2023-01-10"
                             :dataset "production"
@@ -8,6 +10,6 @@
 
 (sanity/query client
               {:query "raw groq"
-               :parameters {:startDate "2021-01-01T00:00:01Z"
-                            :endDate "2022-01-01T00:00:01Z"}})
+               :params {:startDate "2021-01-01T00:00:01Z"
+                        :endDate "2022-01-01T00:00:01Z"}})
 ;; returns edn
