430082175970d2d94fa82e80fd674c63d7beeb44
Teodor Heggelund <git@teod.eu>
2023-01-10 16:47:29 +0100

    Fix: :params, not :parameters

--- src/teodorlu/sanity_edn.clj
+++ src/teodorlu/sanity_edn.clj
@@ -11,18 +11,18 @@
 
 (defn query
   "Run a GROQ query against a Sanity database."
-  [client {:keys [query parameters]}]
+  [client {:keys [query params]}]
   (assert (::client-valid? client))
   (assert query)
   (let [{:keys [project-id api-version dataset secret]} client
         url (str "https://" project-id ".api.sanity.io/" api-version "/data/query/" dataset)
         headers {"Authorization" (str "Bearer " secret)
                  "Content-Type" "application/json"}
+        ;; _ (println (json/generate-string {:query query :params params} {:pretty true}))
         response (-> (http/request {:method :post
                                     :url url
                                     :headers headers
-                                    :body (json/generate-string {:query query
-                                                                 :parameters (or parameters {})})})
+                                    :body (json/generate-string {:query query :params params})})
                      deref)]
     (assert (= (:status response) 200))
-    (json/parse-string (:body response))))
+    (json/parse-string (:body response) keyword)))
